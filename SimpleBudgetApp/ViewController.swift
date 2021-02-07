ae2e66434169a6b83afb146c01b627e9a9b6f9b5
kurisu-seima <kurisuseima@gmail.com>
2021-02-07 21:34:29 +0900

    Add：新規プロジェクト作成

---
+++ SimpleBudgetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleBudgetApp
+//
+//  Created by 栗須星舞 on 2021/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
