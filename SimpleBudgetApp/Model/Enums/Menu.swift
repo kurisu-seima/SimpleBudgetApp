6ee245317f001ce8ac0d6d7dbfbdee9b22cb90c7
kurisu-seima <kurisuseima@gmail.com>
2021-07-04 23:31:27 +0900

    ディレクトリ整理＋サイドメニューの画像が映らない問題を修正

--- SimpleBudgetApp/Model/Enums/Menu.swift
+++ SimpleBudgetApp/Model/Enums/Menu.swift
@@ -32,13 +32,13 @@ enum Menu: CaseIterable {
     var image: String {
         switch self {
         case .yourMenoy:
-            return "Graph"
+            return "graph"
         case .budgetSetting:
             return "money"
         case .payDay:
             return "setting"
         case .balanceBreakDown:
-            return "Menu-Documents"
+            return "menu-documents"
         case .monthlyRecord:
             return "book"
         }
